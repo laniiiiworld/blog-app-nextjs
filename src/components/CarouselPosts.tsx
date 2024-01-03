3fb5e4b2778b2f98a75abc9023b943f842451315
lani <laniiiiworld@gmail.com>
2024-01-04 00:37:34 +0900

    fix: Change from children prop to content

--- src/components/CarouselPosts.tsx
+++ src/components/CarouselPosts.tsx
@@ -8,11 +8,11 @@ export default async function CarouselPosts() {
   return (
     <section className='my-4 px-4'>
       <h2 className='text-2xl font-semibold mb-2'>You May Like</h2>
-      <MultiCarousel
-        children={posts.map((post) => (
+      <MultiCarousel>
+        {posts.map((post) => (
           <PostCard key={post.path} post={post} />
         ))}
-      />
+      </MultiCarousel>
     </section>
   );
 }
