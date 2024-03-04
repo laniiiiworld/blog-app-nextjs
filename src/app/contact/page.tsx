fffca2b4f39417e2fb0a9a8c63a994dfeecff532
lani <laniiiiworld@gmail.com>
2024-03-04 20:46:31 +0900

    Feat: Implement sorted function by newest/oldest/name on home page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default async function ContactPage() {
   const contactInfo = await getContactInfo();
   return (
-    <section className='flex flex-col items-center gap-2 mt-8'>
+    <section className='flex flex-col items-center gap-2 mt-16'>
       <h2 className='text-2xl font-semibold'>Contact me</h2>
       <p>{contactInfo.email}</p>
       <div className='cursor-pointer hover:text-yellow-400'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from '@/components/Hero';
-import FeaturedPosts from '@/components/FeaturedPosts';
+import AllPosts from '@/components/AllPosts';
+import { getAllPosts } from '@/service/posts';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await getAllPosts();
+
   return (
     <>
       <Hero />
-      <FeaturedPosts />
+      <AllPosts posts={posts} />
     </>
   );
 }
