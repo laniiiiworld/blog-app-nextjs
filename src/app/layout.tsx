015e4b07595c5bbcebe15e89141247f9ef1d302e
lani <laniiiiworld@gmail.com>
2024-03-05 14:57:07 +0900

    Design: Update header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <body
-        className={`${gothic.className} flex flex-col w-full h-screen max-w-screen-xl mx-auto leading-normal font-normal`}
-      >
+      <body className={`${gothic.className} w-full h-screen leading-normal font-normal`}>
         <Header font={oswald} />
-        <main className='grow p-4 pt-12 pb-24'>{children}</main>
+        <main className='max-w-screen-xl mx-auto grow p-4 pt-12 pb-24'>{children}</main>
       </body>
     </html>
   );
