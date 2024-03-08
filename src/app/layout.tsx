58553870cdb05f55d309ccfd548b5b62e753d8c0
lani <laniiiiworld@gmail.com>
2024-03-08 22:14:49 +0900

    Chore: Move metadata from layout to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import Header from '@/components/Header';
-import type { Metadata } from 'next';
 import { Nanum_Gothic, Oswald } from 'next/font/google';
 import './globals.css';
 
 const oswald = Oswald({ subsets: ['latin'] });
 const gothic = Nanum_Gothic({ weight: ['400', '700'], subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: {
-    default: '라니의 블로그',
-    template: '라니의 블로그 | %s',
-  },
-  description: 'FrontEnd 개발자 라니의 블로그',
-  icons: { icon: '/favicon.ico' },
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import Hero from '@/components/Hero';
 import AllPosts from '@/components/AllPosts';
 import { getAllPosts } from '@/service/posts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: '라니의 블로그',
+    template: '라니의 블로그 | %s',
+  },
+  description: 'FrontEnd 개발자 라니의 블로그',
+  icons: { icon: '/favicon.ico' },
+};
 
 export default async function HomePage() {
   const posts = await getAllPosts();
