c99e15c20d75bdafde9b50c38e1054af3d9fc03f
lani <laniiiiworld@gmail.com>
2024-03-10 13:10:57 +0900

    Feat: Implement Google Login using Firebase for commenting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import Header from '@/components/Header';
 import { Nanum_Gothic, Oswald } from 'next/font/google';
 import './globals.css';
+import { AuthContextProvider } from '@/context/AuthContext';
 
 const oswald = Oswald({ subsets: ['latin'] });
 const gothic = Nanum_Gothic({ weight: ['400', '700'], subsets: ['latin'] });
@@ -9,8 +12,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='ko'>
       <body className={`${gothic.className} w-full h-screen leading-normal font-normal`}>
+        <AuthContextProvider>
         <Header font={oswald} />
         <main className='max-w-screen-xl mx-auto grow p-4 pt-12 pb-24'>{children}</main>
+        </AuthContextProvider>
       </body>
     </html>
   );
