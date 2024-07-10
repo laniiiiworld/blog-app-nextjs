f13cd70783a7c3e91cce0d8b7705f98bfb2029c4
lani <laniiiiworld@gmail.com>
2024-07-10 13:38:12 +0900

    Chore: Create metadata on the write page

---
+++ src/app/write/[slug]/layout.tsx
@@ -0,0 +1,26 @@
+import { getPost } from '@/service/post';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
+type Props = {
+  children: React.ReactNode;
+  params: { slug: string };
+};
+
+export async function generateMetadata({ params: { slug } }: Props): Promise<Metadata> {
+  try {
+    const post = await getPost(slug);
+    if (!post) notFound();
+    const { title, description } = post;
+    return {
+      title: `(작성중) ${title}`,
+      description,
+    };
+  } catch (error) {
+    notFound();
+  }
+}
+
+export default async function PostPageLayout({ children }: Props) {
+  return <>{children}</>;
+}
--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,5 +1,11 @@
 import PostForm from '@/components/PostForm';
 import { PostFormContextProvider } from '@/context/PostFormContext';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '새 글 작성',
+  description: '새로운 글을 작성해보세요.',
+};
 
 export default function AddPage() {
   return (
